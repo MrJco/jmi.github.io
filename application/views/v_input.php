3452e4492cdccee741a08d761ad1cab063faa5a0
MrJco <jokomaulanaibrahim32@gmail.com>
2021-04-09 02:27:21 +0700

    MrJco

---
+++ application/views/v_input.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Membuat CRUD </title>
+</head>
+<body>
+<center>
+<form action="http://127.0.0.1/ci/index.php/welcome/tambah_aksi" method="post">
+<table style="margin:20px auto;">
+<tr><td>Nama Mahasiswa</td><td>:</td><td><input type="text" name="nama"></td></tr>
+<tr><td>NIM</td><td>:</td><td><input type="text" name="nim"></td></tr>
+<tr><td>EMAIL</td><td>:</td><td><input type="text" name="email"></td></tr>
+<tr><td></td><td colspan="2"><input type="submit" value="input"></td></tr>
+</tr>
+</table>
+</from>
+</center>
+</body>
+</html>
